0c08ad095030fed240b168982591d9c30ec3a9e9
bayoticstudios <abdullahishobaloju@gmail.com>
2023-07-27 22:51:32 +0100

    courses carousel wip

--- frontend/src/components/Parallax.js
+++ frontend/src/components/Parallax.js
@@ -1,82 +1,183 @@
+/************************************
+1. If you want to add or remove items you will need to change a variable called $slide-count in the CSS *minimum 3 slides
+
+2. if you want to change the dimensions of the slides you will need to edit the slideWidth variable here 👇 and the $slide-width variable in the CSS.
+************************************/
+
 import { useState, useEffect } from "react";
 
-const Parallax = () => {
+const slideWidth = 30;
 
-    const items = [
+const _items = [
     {
-        icon:"face",
-        copy:'01. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },{
-        icon:"pets",
-        copy:'02. Sed do eiusmod tempor incididunt ut labore.'
-    },{
-        icon:"stars",
-        copy:'03. Consectetur adipiscing elit.'
-    },{
-        icon:"invert_colors",
-        copy:'04. Ut enim ad minim veniam, quis nostrud exercitation.'
-    },{
-        icon:"psychology",
-        copy:'05. Llamco nisi ut aliquip ex ea commodo consequat.'
-    },{
-        icon:"brightness_7",
-        copy:'06. Misi ut aliquip ex ea commodo consequat.'
-    }
+        player: {
+            title: 'Certified ScrumMaster',
+            carouselList: [
+                '30 hours of interactive live sessions with experienced instructors and experiential workshops.',
+                'Receive training from Scrum Alliance Certified Scrum Trainers (CSTs) and obtain 14 (PDUs) and (SEUs).'
+            ],
+            image: [
+                
+            ],
+
+            courseDuration: '30',
+            rating: '',
+        },
+    },
+    {
+        player: {
+            title: "Ronnie O'Sullivan",
+            desc: "Ronald Antonio O'Sullivan is a six-time world champion and is the most successful player in the history of snooker.",
+            image: 'https://i.postimg.cc/qBGQNc37/ro-slider.jpg',
+        },
+    },
+    {
+        player: {
+            title: 'Shane Van Boening',
+            desc: 'The "South Dakota Kid" is hearing-impaired and uses a hearing aid, but it has not limited his ability.',
+            image: 'https://i.postimg.cc/cHdMJQKG/svb-slider.jpg',
+        },
+    },
+    {
+        player: {
+            title: 'Mike Sigel',
+            desc: 'Mike Sigel or "Captain Hook" as many like to call him is an American professional pool player with over 108 tournament wins.',
+            image: 'https://i.postimg.cc/C12h7nZn/ms-1.jpg',
+        },
+    },
+    {
+        player: {
+            title: 'Willie Mosconi',
+            desc: 'Nicknamed "Mr. Pocket Billiards," Willie Mosconi was among the first Billiard Congress of America Hall of Fame inductees.',
+            image: 'https://i.postimg.cc/NfzMDVHP/willie-mosconi-slider.jpg',
+        },
+    },
 ];
 
-const Card = (props) => {
-  return (
-    <li className="card">
-      <span class="material-icons">{props.icon}</span>
-      <p>{props.copy}</p>
-    </li>
-  )
-}
+const length = _items.length;
+_items.push(..._items);
 
-  const [moveClass, setMoveClass] = useState('');
-  const [carouselItems, setCarouselItems] = useState(items);
-  
-  useEffect(() => {
-    document.documentElement.style.setProperty('--num', carouselItems.length);
-  }, [carouselItems])
-  
-  const handleAnimationEnd = () => {
-    if(moveClass === 'prev'){
-      shiftNext([...carouselItems]);
-    }else if(moveClass === 'next'){
-      shiftPrev([...carouselItems]);
+const sleep = (ms = 0) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const createItem = (position, idx) => {
+    const item = {
+        styles: {
+            transform: `translateX(${position * slideWidth}rem)`,
+        },
+        player: _items[idx].player,
+    };
+
+    switch (position) {
+        case length - 1:
+        case length + 1:
+            item.styles = {...item.styles, filter: 'grayscale(1)'};
+            break;
+        case length:
+            break;
+        default:
+            item.styles = {...item.styles, opacity: 0};
+            break;
     }
-    setMoveClass('')
-  }
-  
-  const shiftPrev = (copy) => {
-    let lastcard = copy.pop();
-    copy.splice(0, 0, lastcard);
-    setCarouselItems(copy);
-  }
-  
-  const shiftNext = (copy) => {
-    let firstcard = copy.shift();
-    copy.splice(copy.length, 0, firstcard);
-    setCarouselItems(copy);
-  }
-  
-  return (
-    <div className="carouselwrapper module-wrapper">
-      <div className="ui">
-        <button onClick={() => setMoveClass('next')} className="prev">
-          <span className="material-icons">chevron_left</span>
-        </button>
-        <button onClick={() => setMoveClass('prev')} className="next">
-          <span className="material-icons">chevron_right</span>
-        </button>
-      </div>
-      <ul onAnimationEnd={handleAnimationEnd} className={`${moveClass} carousel`}>
-        {carouselItems.map((t, index) => 
-          <Card key={t.copy + index} icon={t.icon} copy={t.copy} />
-        )}
-      </ul>
-    </div>
-  )
-}
+
+    return item;
+};
+
+const CarouselSlideItem = ({pos, idx, activeIdx}) => {
+    const item = createItem(pos, idx, activeIdx);
+
+    return (
+        <li className="carousel__slide-item" style={item.styles}>
+            <div className="border carousel__slide-item-img-link">
+                <div className="border">
+                    <div className="border carousel-header">
+                        <h4>Certified ScrumMaster</h4>
+                    </div>
+                </div>
+                {/* <img src={item.player.image} alt={item.player.title} /> */}
+            </div>
+            <div className="carousel-slide-item__body">
+                <h4>{item.player.title}</h4>
+                <p>{item.player.desc}</p>
+            </div>
+        </li>
+    );
+};
+
+const keys = Array.from(Array(_items.length).keys());
+
+const Parallax = () => {
+    const [items, setItems] = useState(keys);
+    const [isTicking, setIsTicking] = useState(false);
+    const [activeIdx, setActiveIdx] = useState(0);
+    const bigLength = items.length;
+
+    const prevClick = (jump = 1) => {
+        if (!isTicking) {
+            setIsTicking(true);
+            setItems((prev) => {
+                return prev.map((_, i) => prev[(i + jump) % bigLength]);
+            });
+        }
+    };
+
+    const nextClick = (jump = 1) => {
+        if (!isTicking) {
+            setIsTicking(true);
+            setItems((prev) => {
+                return prev.map(
+                    (_, i) => prev[(i - jump + bigLength) % bigLength],
+                );
+            });
+        }
+    };
+
+    const handleDotClick = (idx) => {
+        if (idx < activeIdx) prevClick(activeIdx - idx);
+        if (idx > activeIdx) nextClick(idx - activeIdx);
+    };
+
+    useEffect(() => {
+        if (isTicking) sleep(300).then(() => setIsTicking(false));
+    }, [isTicking]);
+
+    useEffect(() => {
+        setActiveIdx((length - (items[0] % length)) % length) // prettier-ignore
+    }, [items]);
+
+    return (
+        <div className="carousel__wrap">
+            <div className="carousel__inner">
+                <button className="carousel__btn carousel__btn--prev" onClick={() => prevClick()}>
+                    <i className="carousel__btn-arrow carousel__btn-arrow--left" />
+                </button>
+                <div className="carousel__container">
+                    <ul className="carousel__slide-list">
+                        {items.map((pos, i) => (
+                            <CarouselSlideItem
+                                key={i}
+                                idx={i}
+                                pos={pos}
+                                activeIdx={activeIdx}
+                            />
+                        ))}
+                    </ul>
+                </div>
+                <button className="carousel__btn carousel__btn--next" onClick={() => nextClick()}>
+                    <i className="carousel__btn-arrow carousel__btn-arrow--right" />
+                </button>
+                <div className="carousel__dots">
+                    {items.slice(0, length).map((pos, i) => (
+                        <button
+                            key={i}
+                            onClick={() => handleDotClick(i)}
+                            className={i === activeIdx ? 'dot active' : 'dot'}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
 export default Parallax
